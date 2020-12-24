ce9693d3fc4c45861c9a23d7d6c892b6b8cd90ad
gdmora <gdmora@espol.edu.ec>
2020-12-23 21:46:40 -0500

    Creación de clase Persona

---
+++ TalllerRefactoring/src/modelos/Persona.java
@@ -0,0 +1,50 @@
+package modelos;
+
+public class Persona{
+    public String nombre;
+    public String apellido;
+    public int edad;
+    public String direccion;
+    public String telefono;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+}
